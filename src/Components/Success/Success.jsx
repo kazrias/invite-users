060cc717b3a31b05f8fe9479bc868ae7ce5f0528
kazrias <ka6rias@gmail.com>
2023-05-25 09:48:11 +0400

    added correct success's path

--- src/Components/Success/Success.jsx
+++ src/Components/Success/Success.jsx
@@ -2,7 +2,7 @@ import './Success.scss'
 function Success({ count }){
   return (
     <div className="success-block">
-      <img src="../../src/assets/success.svg" alt="Success" />
+      <img src="./assets/success.svg" alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем({count})  пользователям отправлено приглашение.</p>
       <button onClick={()=>window.location.reload()} className="send-invite-btn">Назад</button>
